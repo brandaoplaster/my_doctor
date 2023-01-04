486f59a63976431b3b53c11e223366314996006e
brandaoplaster <brandaoplaster@gmail.com>
2023-01-03 22:38:21 -0300

    feat: Adjusting user controller

--- lib/my_doctor_web/controllers/user_controller.ex
+++ lib/my_doctor_web/controllers/user_controller.ex
@@ -1,18 +1,18 @@
 defmodule MyDoctorWeb.UserController do
   use MyDoctorWeb, :controller
 
-  alias MyDoctor.Accounts
-  alias MyDoctor.Accounts.User
+  alias MyDoctor.Accounts.Entities.User
+  alias MyDoctor.Accounts.Repositories.AccountRepository
 
   action_fallback MyDoctorWeb.FallbackController
 
   def index(conn, _params) do
-    users = Accounts.list_users()
+    users = AccountRepository.list_users()
     render(conn, "index.json", users: users)
   end
 
   def create(conn, %{"user" => user_params}) do
-    with {:ok, %User{} = user} <- Accounts.create_user(user_params) do
+    with {:ok, %User{} = user} <- AccountRepository.create_user(user_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.user_path(conn, :show, user))
@@ -21,22 +21,22 @@ defmodule MyDoctorWeb.UserController do
   end
 
   def show(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
+    user = AccountRepository.get_user!(id)
     render(conn, "show.json", user: user)
   end
 
   def update(conn, %{"id" => id, "user" => user_params}) do
-    user = Accounts.get_user!(id)
+    user = AccountRepository.get_user!(id)
 
-    with {:ok, %User{} = user} <- Accounts.update_user(user, user_params) do
+    with {:ok, %User{} = user} <- AccountRepository.update_user(user, user_params) do
       render(conn, "show.json", user: user)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    user = Accounts.get_user!(id)
+    user = AccountRepository.get_user!(id)
 
-    with {:ok, %User{}} <- Accounts.delete_user(user) do
+    with {:ok, %User{}} <- AccountRepository.delete_user(user) do
       send_resp(conn, :no_content, "")
     end
   end
