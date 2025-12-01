98d3aba106fcdec64b5df9d8f6c998ed75c8bfdf
brandaoplaster <brandaoplaster@gmail.com>
2025-11-30 21:15:53 -0300

    chore: Add user factory

---
+++ test/support/factories/user_factory.ex
@@ -0,0 +1,14 @@
+defmodule MyDoctor.UserFactory do
+  alias MyDoctor.Accounts.Schemas.User
+
+  defmacro __using__(_opts) do
+    quote do
+      def user_factory do
+        %User{
+          email: Faker.Internet.email(),
+          hashed_password: Bcrypt.hash_pwd_salt("password123")
+        }
+      end
+    end
+  end
+end
