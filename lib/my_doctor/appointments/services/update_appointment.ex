374220b472edab626181f7bb5ad39676c3f0792f
brandaoplaster <brandaoplaster@gmail.com>
2025-12-04 22:31:51 -0300

    feat: Add update appointment service

---
+++ lib/my_doctor/appointments/services/update_appointment.ex
@@ -0,0 +1,24 @@
+defmodule MyDoctor.Appointments.Services.UpdateAppointment do
+  @moduledoc """
+  Appointment update service.
+  """
+
+  alias MyDoctor.Appointments.Schemas.Appointment
+  alias MyDoctor.Appointments.Repositories.AppointmentRepository
+
+  def execute(id, attrs) do
+    appointment = AppointmentRepository.get!(id)
+
+    attrs = Map.update!(attrs, "date", &format_hour/1)
+
+    appointment
+    |> Appointment.changeset(attrs)
+    |> AppointmentRepository.update()
+  end
+
+  defp format_hour(date) do
+    date = NaiveDateTime.from_iso8601!(date)
+
+    %NaiveDateTime{date | minute: 0, second: 0, microsecond: {0, 0}}
+  end
+end
